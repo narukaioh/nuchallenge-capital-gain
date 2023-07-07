16d443c3fd23dc240d5ccfc3ac6c5444c66567ef
Ju Dantas <jucienyds@gmail.com>
2023-07-06 22:07:55 -0300

    fix: Correct response in getTax test

--- src/getTax.test.js
+++ src/getTax.test.js
@@ -5,7 +5,7 @@ import { getTax } from './getTax'
 describe('getTax', () => {
   test('should return a tax object for each operation', () => {
     const operations = [[{ 'operation': '', 'unit-cost': 0, 'quantity': 0 }]]
-    const expected = [{ 'tax': expect.any(Number) }]
+    const expected = [[{ 'tax': expect.any(Number) }]]
 
     expect(getTax(operations)).toEqual(expected)
   })
