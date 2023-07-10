bf494dc8d05d3f02225d4f1c7329cd990b6ffdd9
Ju Dantas <jucienyds@gmail.com>
2023-07-10 14:44:39 -0300

    refactor: Refactor calculateTaxes function

--- src/services/calculateTaxes.js
+++ src/services/calculateTaxes.js
@@ -1,61 +1,86 @@
 const isWithinLimit = ({ quantity, cost }) => (quantity * cost) >= 20000
-const hasLoss = ({ media, cost }) => media > cost
-const calculateTotal = ({ media, cost, quantity }) => (cost - media) * quantity
+
+const hasLoss = ({ averageCost, cost }) => averageCost > cost
+
+const calculateTotal = ({ averageCost, cost, quantity }) => (cost - averageCost) * quantity
+
 const calculateTax = (value) => value * 0.2
-const isSellOperation = operation => operation === 'sell'
+
+const isSellOperation = (operation) => operation === 'sell'
+
+const calculateWeightedAverage = (historic) => {
+  const amount = historic.reduce((acc, cur) => acc + cur.quantity * cur.cost, 0)
+  const stocks = historic.reduce((acc, cur) => acc + cur.quantity, 0)
+  return amount / stocks
+}
+
+const isDeductible = (total, loss) => total <= loss
+
+const hasStocks = (stocks) => stocks > 0
+
+const updateState = (state, { operation, quantity, cost }) => {
+  const actualQuantity = isSellOperation(operation) ? -quantity : quantity
+
+  const historic = [...state.historic, { quantity, cost }]
+  const averageCost = !isSellOperation(operation)
+    ? calculateWeightedAverage(historic)
+    : state.averageCost
+
+  return {
+    ...state,
+    historic,
+    stocks: state.stocks + actualQuantity,
+    amount: state.amount + actualQuantity * cost,
+    averageCost
+  }
+}
 
 export const calculateTaxes = (operations) => {
-  let initialState = { historic: [], media: 0, amount: 0, stocks: 0, loss: 0 }
-
-  const updateState = ({ operation, quantity, cost }) => {
-    const actualQuantity = (operation === 'sell' ? -quantity : quantity)
-
-    let updatedMedia = initialState.media
-
-    if (operation === 'buy') {
-      initialState.historic.push({ quantity, cost })
-      const amount = initialState.historic.reduce((acc, cur) => acc + (cur.quantity * cur.cost), 0)
-      const stocks = initialState.historic.reduce((acc, cur) => acc + cur.quantity, 0)
-      updatedMedia = amount / stocks
-    }
-
-    return {
-      ...initialState,
-      stocks: initialState.stocks + actualQuantity,
-      amount: initialState.amount + (actualQuantity * cost),
-      media: updatedMedia
-    }
+  let state = {
+    historic: [],
+    averageCost: 0,
+    amount: 0,
+    stocks: 0,
+    loss: 0
   }
 
-  return operations.map(({ operation, quantity, 'unit-cost': cost }) => {
-    initialState = { ...updateState({ operation, cost, quantity }) }
+  const calculateTaxForOperation = ({ operation, quantity, 'unit-cost': cost }) => {
+    state = updateState(state, { operation, quantity, cost })
 
-    if (isSellOperation(operation) &&
-      !hasLoss({ media: initialState.media, cost }) &&
+    if (
+      isSellOperation(operation) &&
+      !hasLoss({ averageCost: state.averageCost, cost }) &&
       isWithinLimit({ quantity, cost })
     ) {
-      let total = calculateTotal({ media: initialState.media, cost, quantity })
+      let total = calculateTotal({
+        averageCost: state.averageCost,
+        cost,
+        quantity
+      })
 
-      if (total <= initialState.loss) {
-        initialState.loss -= total
+      if (isDeductible(total, state.loss)) {
+        state.loss -= total
         return { tax: 0 }
       }
 
-      total -= initialState.loss
-      initialState.loss = 0
+      total -= state.loss
+      state.loss = 0
 
-      if (initialState.stocks <= 0) {
-        initialState.historic = []
-        initialState.media = 0
+      if (!hasStocks(state.stocks)) {
+        state.historic = []
+        state.averageCost = 0
       }
 
       return { tax: calculateTax(total) }
     }
 
-    initialState.loss = (initialState.media !== cost && operation === 'sell')
-      ? initialState.loss + (quantity * (initialState.media - cost))
-      : 0
+    state.loss =
+      state.averageCost !== cost && isSellOperation(operation)
+        ? state.loss + quantity * (state.averageCost - cost)
+        : 0
 
     return { tax: 0 }
-  })
+  }
+
+  return operations.map(calculateTaxForOperation)
 }
