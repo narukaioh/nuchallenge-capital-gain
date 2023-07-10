8c9d2635355ef3c90e850bbbd88f5f1045149191
Ju Dantas <jucienyds@gmail.com>
2023-07-10 18:10:05 -0300

    chore: Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import { getTax } from './getTax'
-import { readFile } from './utils/readFile'
-import { stringToList } from './utils/stringToList'
+import { getTaxes } from './services/getTaxes'
+import { readFile } from './components/readFile'
+import { stringToList } from './components/stringToList'
 
 const init = async () => {
   const filePath = process.argv[2]
   const strings = await readFile(filePath)
   const lists = stringToList(strings)
-  const result = getTax(lists)
+  const result = getTaxes(lists)
 
   console.log(result)
 }
